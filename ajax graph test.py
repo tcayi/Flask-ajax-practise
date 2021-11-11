2de4127bdb01205dd486fbb53e9f36e7a86c5c6e
tcayi <78546474+tcayi@users.noreply.github.com>
2021-11-11 09:50:08 +0800

    Update ajax graph test.py

--- ajax graph test.py
+++ ajax graph test.py
@@ -12,13 +12,15 @@ def index():
     a = randrange(100)
     b = randrange(100)
 
-    # random letter
+    # random letter //Actual aim isn't randomly generated, random is just a test
     c = random.choice(string.ascii_letters)
     print(a,b,c)
 
     data= {"y1": a, "y2": b, "x": c}
+    datap = json.dumps(datap)
+    print(datap)
 
-    return render_template('test.html', a=a, b=b, c=c, data=data)
+    return render_template('test.html', a=a, b=b, c=c, datap=datap)
 
 if __name__ == '__main__':
     app.debug = False
