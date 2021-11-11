699ede2a720f2b30ef5f100565a98c6b0d27ac97
tcayi <78546474+tcayi@users.noreply.github.com>
2021-11-11 10:36:32 +0800

    Update ajax graph test.py

--- ajax graph test.py
+++ ajax graph test.py
@@ -12,13 +12,20 @@ def index():
     a = randrange(100)
     b = randrange(100)
 
-    # random letter //Actual aim isn't randomly generated, random is just a test
+    # random letter //My eventual aim isn't randomly generated data, random is to test if values can be properly sent back to frontend
     c = random.choice(string.ascii_letters)
     print(a,b,c)
 
     data= {"y1": a, "y2": b, "x": c}
+    
+    # Form the generated data into a json format
     datap = json.dumps(datap)
     print(datap)
+    
+    # Q: Am I supposed to use another flask route to recieve the request from the html front end?
+    # If so, how to ensure that I return the updated json object to frontend? 
+    # (I know a possible method is through (if request method == post), but im not sure about afterwards
+    
 
     return render_template('test.html', a=a, b=b, c=c, datap=datap)
 
